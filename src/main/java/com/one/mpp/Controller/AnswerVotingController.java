0bd32663631b9f64457786ca9646b0c5093100fd
Aaron Gezai <aaaron769@gmail.com>
2019-06-05 14:53:02 -0500

    commented

--- src/main/java/com/one/mpp/Controller/AnswerVotingController.java
+++ src/main/java/com/one/mpp/Controller/AnswerVotingController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 // the rest controller
-
+// The Rest controller
 
 @RestController
 @CrossOrigin
